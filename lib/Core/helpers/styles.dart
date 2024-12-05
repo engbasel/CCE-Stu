44ad536685a1c4c4d839970c32da2ee70e094bc1
ranamohyy <ranaamr22@gmail.com>
2024-12-05 03:21:18 +0200

    add styles file

---
+++ lib/Core/helpers/styles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../manager/ColorsManager.dart';
+
+  const kTextStyle14 = TextStyle(
+    color: ColorsManager.coustomLabelTextColor,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+const kTextStyle15 = TextStyle(
+  color: ColorsManager.coustomLabelTextColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 15, // You can adjust the font size
+);
+const kTextStyle11grey = TextStyle(
+  fontSize: 11.5,
+  color: Colors.grey,
+
+);
+const kTextStyle14underline = TextStyle(
+  fontSize: 14,
+  color: ColorsManager.coustomLabelTextColor,
+  fontWeight: FontWeight.bold,
+  decoration: TextDecoration.underline,
+
+);
+
+
